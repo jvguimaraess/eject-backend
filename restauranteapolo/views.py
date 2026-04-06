59fe6c4f1820a9ac227b3319f3ba6133a464e5fe
João <joao_jvga@icloud.com>
2026-04-05 23:16:36 -0300

    feat: integração dos templates HTML e arquivos estáticos

--- restauranteapolo/views.py
+++ restauranteapolo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'restauranteapolo/index.html')
+
+def blog(request):
+    return render(request, 'restauranteapolo/blog.html')
+
+def artigo(request):
+    return render(request, 'restauranteapolo/artigo.html')
+
+def sobre(request):
+    return render(request, 'restauranteapolo/sobre.html')
